83e6d2d04624919811bf26879d5de74109990033
Mark <Mark@windows10.microdone.cn>
2016-11-21 09:56:45 +0800

    数据处理完毕

--- 成招网报/数据处理.sql
+++ 成招网报/数据处理.sql
@@ -1,5 +1,21 @@
+--delete from t_jtbmdwhd where qxdm='3126'
+--delete from BM_HDSZ_ZY where qxdm='3126'
+
+311302 1001
+select * from t_threshold where id='3121'
+update t_threshold set maxno=500 where id='3121'
+
+select * from t_threshold where id='3113'
+update t_threshold set maxno=1500 where id='3113'
+
+
+select * from t_jtbmdwhd where qxdm='3113'
+select * from BM_HDSZ_ZY where qxdm='3113' and lsh > '00998'
+
 select * from bm_sfzh  where qxdm='3104' and cjyh like '0422%'
 '0422'
+
+select * from t_student_assist_info 
 
 select * from bm_sfzh where bmd='310507' and sfzh not in (select sfzh from t_student_info) order by qxdm, bmd, sfzh
 
@@ -7,6 +23,55 @@ select * from t_student_info where sfzh not in (select sfzh from bm_sfzh)
 
 select xm, sfzh from bm_sfzh where sfzh like '500239198707168%'
 
+SELECT ASCII(substring('50010119941008152x', 18, 1))
+50010119941008152x
+select * from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120
+
+select * from t_student_baseinfo where sfzh='520221199106284087'
+
+update t_student_baseinfo set sfzh=upper(sfzh) where sfzh in (select sfzh from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120)
+update t_student_assist_info set sfzh=upper(sfzh) where sfzh in (select sfzh from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120)
+update t_society_student_photo set sfzh=upper(sfzh) where sfzh in (select sfzh from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120)
+update t_zyk_temp set sfzh=upper(sfzh) where sfzh in (select sfzh from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120)
+update t_PayFeeOrder set idCardNumber=upper(idCardNumber) where idCardNumber in (select sfzh from t_student_baseinfo where ASCII(substring(sfzh, 18, 1))=120)
+
+select * from t_student_baseinfo where sfzh like '500230199509034384%'
+select * from t_student_assist_info where sfzh like '500230199509034384%'
+update t_student_baseinfo set sfzh='50022519840118473X' where sfzh = '50022519840118473x'
+update t_student_assist_info set sfzh='50022519840118473X' where sfzh = '50022519840118473x'
+update t_society_student_photo set sfzh='50022519840118473X' where sfzh = '50022519840118473x'
+update t_zyk_temp set sfzh='50022519840118473X' where sfzh = '50022519840118473x'
+update t_PayFeeOrder set idCardNumber='50022519840118473X' where idCardNumber = '50022519840118473x'
+
+
+select * from t_student_baseinfo where sfzh like '500233199808080064%'
+select xm, sfzh from bm_sfzh where sfzh like '500233199808080064%' and sfzh not in (select sfzh from t_student_info)
+
+delete from bm_sfzh where sfzh like '511324199519031918%'
+delete from bm_sfzh where sfzh like '500233199808080064%'
+delete from bm_sfzh where sfzh like '500233199911285682%'
+delete from bm_sfzh where sfzh like '500242199706207518%'
+delete from bm_sfzh where sfzh like '500230199906186218%'
+delete from bm_sfzh where sfzh like '500233199911285682%'
+delete from bm_sfzh where sfzh like '511323199512150217%'
+delete from bm_sfzh where sfzh like '500101199712158254%'
+delete from bm_sfzh where sfzh like '522426199611100853%'
+delete from bm_sfzh where sfzh like '510923199708046915%'
+delete from bm_sfzh where sfzh like '500109200003092324%'
+delete from bm_sfzh where sfzh like '522423199706286014%'
+delete from bm_sfzh where sfzh like '500384199809022830%'
+delete from bm_sfzh where sfzh like '522224199606155071%'
+delete from bm_sfzh where sfzh like '510502200004018715%'
+update t_student_baseinfo set yxdz='1614248166@qq.com' where sfzh='500230199509034384' and yxdz='164248166@qq.com'
+delete from bm_sfzh where sfzh like '500243199904050669%'
+delete from bm_sfzh where sfzh like '500227199905227315%'
+delete from bm_sfzh where sfzh like '500109199808287561%'
+delete from bm_sfzh where sfzh like '500109199907088621%'
+delete from bm_sfzh where sfzh like '500226199903080026%'
+delete from bm_sfzh where sfzh like '500230199808304401%'
+delete from bm_sfzh where sfzh like '500228199506303735%'
+delete from bm_sfzh where sfzh like '500102199307047098%'
+delete from bm_sfzh where sfzh like '51302119940917255X%'
 delete from bm_sfzh where sfzh like '500239198707168%'
 delete from bm_sfzh where sfzh like '51222219731222%'
 delete from bm_sfzh where sfzh like '50038419990906005X%'
